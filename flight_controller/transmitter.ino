7f325a8dfb2ffe03b3f491994ef1072097341193
Simon Khan <simonhkhan@users.noreply.github.com>
2017-02-26 10:20:33 -0800

    Changed switch transmitter to pot transmitter

--- flight_controller/transmitter.ino
+++ flight_controller/transmitter.ino
@@ -7,82 +7,72 @@ int write_throttle_pin = 9;
 int write_yaw_pin = 10;
 
 int pwm_value_throttle;
-int pwm_value_yaw;          
+int pwm_value_yaw;
+int pwm_motor_val;          
 
-int transmitter_highest = 13;
-int transmitter_high = 12;
-int transmitter_medium = 11;
-int transmitter_low = 7;
-int transmitter_lowest = 6;
+byte read_motor_val = 11;
 
 int throttle_signal = 4;
 int yaw_signal = 2;
 
-int write_signal = 100;
+//int write_signal = 100;
+
+int readPotValue() {
+    int sensorValue = analogRead(A0);
+    int mappedValue = map(sensorValue, 0, 1001, 100, 255);
+    return mappedValue;
+}
+
+
+void writeSignalValues(int write_signal) {
+  
+      if (digitalRead(throttle_signal) == HIGH)
+      {
+        analogWrite(write_throttle_pin, write_signal);
+        pwm_value_throttle = pulseIn(read_throttle_pin, HIGH);
+        Serial.println("Throttle Signal");
+        Serial.println(pwm_value_throttle);
+        Serial.println(write_signal);
+
+      }
+      if (digitalRead(yaw_signal) == HIGH)
+      {
+        analogWrite(write_yaw_pin, write_signal);
+        pwm_value_yaw = pulseIn(read_yaw_pin, HIGH);
+        Serial.println("Yaw Signal");
+        Serial.println(pwm_value_yaw);
+        Serial.println(write_signal);
+
+      }
+}
 
 void setup() {  
   // put your setup code here, to run once:\\
   pinMode(read_yaw_pin, INPUT);
   pinMode(read_throttle_pin, INPUT);
+  pinMode(read_motor_val, INPUT);
+
 
   pinMode(write_throttle_pin, OUTPUT);
   pinMode(write_yaw_pin, OUTPUT);
-
-  //PINS FOR TRANSMITTER
-  pinMode(transmitter_highest, INPUT_PULLUP);           
-  pinMode(transmitter_high, INPUT_PULLUP);           
-  pinMode(transmitter_medium, INPUT_PULLUP);           
-  pinMode(transmitter_low, INPUT_PULLUP);           
-  pinMode(transmitter_lowest, INPUT_PULLUP); 
 
   //PINS FOR WRITE SIGNAL MODE
   pinMode(throttle_signal, INPUT_PULLUP);           
   pinMode(yaw_signal, INPUT_PULLUP);           
 
-  
   Serial.begin(9600);
 }
 
 void loop() {
-        delay(1000); 
         Serial.println();
+        delay(500);
 
-        write_signal = 100;
+        int write_signal = readPotValue();
 
-        if (digitalRead(transmitter_highest) == HIGH)
-        {
-          write_signal = 250;
-        }
-        if (digitalRead(transmitter_high) == HIGH)
-        {
-          write_signal = 220;
-        }
-        if (digitalRead(transmitter_medium) == HIGH)
-        {
-          write_signal = 180;
-        }
-        if (digitalRead(transmitter_low) == HIGH)
+        writeSignalValues(write_signal);
+
+//        pwm_motor_val = pulseIn(read_motor_val, HIGH);
+//        Serial.println("Motor Value");
+//        Serial.println(pwm_motor_val);
         
-        {
-          write_signal = 150;
-        }
-        if (digitalRead(transmitter_lowest) == HIGH)
-        {
-          write_signal = 130;
-        }
-        
-        if (digitalRead(throttle_signal) == HIGH)
-        {
-          analogWrite(write_throttle_pin, write_signal);
-          pwm_value_throttle = pulseIn(read_throttle_pin, HIGH);
-          Serial.println("Throttle Signal");
-          Serial.println(pwm_value_throttle);
-        }
-        if (digitalRead(yaw_signal) == HIGH)
-        {
-          analogWrite(write_yaw_pin, write_signal);
-          pwm_value_yaw = pulseIn(read_yaw_pin, HIGH);
-          Serial.println("Yaw Signal");
-          Serial.println(pwm_value_yaw);
-        }
 }
